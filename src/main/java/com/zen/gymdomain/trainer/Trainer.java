d0e85c8990943fc9a4a3a6b307e694768f98a310
David <depch47@gmail.com>
2022-05-19 20:42:21 -0500

    Implementing Trainer AR events

--- src/main/java/com/zen/gymdomain/trainer/Trainer.java
+++ src/main/java/com/zen/gymdomain/trainer/Trainer.java
@@ -1,11 +1,42 @@
 package com.zen.gymdomain.trainer;
 
 import co.com.sofka.domain.generic.AggregateEvent;
+import co.com.sofka.domain.generic.DomainEvent;
+import com.zen.gymdomain.trainer.entities.Client;
+import com.zen.gymdomain.trainer.entities.Routine;
+import com.zen.gymdomain.trainer.events.TrainerCreated;
 import com.zen.gymdomain.trainer.values.TrainerID;
 
-public class Trainer extends AggregateEvent<TrainerID> {
+import java.util.List;
+import java.util.Set;
 
-    public Trainer(TrainerID entityId) {
+public class Trainer extends AggregateEvent<TrainerID> {
+    protected Set<Client> clientSet;
+    protected Routine routine;
+
+    public Trainer(TrainerID entityId, Routine routine) {
         super(entityId);
+        appendChange(new TrainerCreated(entityId, routine)).apply();
     }
+
+    private Trainer(TrainerID entityId) {
+        super(entityId);
+        subscribe(new TrainerChange(this));
+    }
+
+    public static Trainer from(TrainerID trainerID, List<DomainEvent> domainEvents) {
+        Trainer trainer = new Trainer(trainerID);
+        domainEvents.forEach(trainer::applyEvent);
+        return trainer;
+    }
+
+    public void addClient(Client client) {
+        this.clientSet.add(client);
+    }
+
+    public void removeClient(Client client) {
+        this.clientSet.remove(client);
+    }
+
+
 }
--- src/main/java/com/zen/gymdomain/trainer/TrainerChange.java
+++ src/main/java/com/zen/gymdomain/trainer/TrainerChange.java
@@ -1,4 +1,15 @@
 package com.zen.gymdomain.trainer;
 
-public class TrainerChange {
+import co.com.sofka.domain.generic.EventChange;
+import com.zen.gymdomain.trainer.events.TrainerCreated;
+
+import java.util.HashSet;
+
+public class TrainerChange extends EventChange {
+    TrainerChange(Trainer trainer) {
+        apply((TrainerCreated event) -> {
+            trainer.routine = event.getRoutine();
+            trainer.clientSet = new HashSet<>();
+        });
+    }
 }
