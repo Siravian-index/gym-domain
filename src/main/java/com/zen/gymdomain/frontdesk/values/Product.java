71709d4f80712d3129cab1085be8b76f7c3dd35d
David <depch47@gmail.com>
2022-05-20 10:28:36 -0500

    Implementing VO in FrontDesk

--- src/main/java/com/zen/gymdomain/frontdesk/values/Product.java
+++ src/main/java/com/zen/gymdomain/frontdesk/values/Product.java
@@ -1,4 +1,18 @@
 package com.zen.gymdomain.frontdesk.values;
 
-public class Product {
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Product implements ValueObject<String> {
+    private final String value;
+
+    public Product(String value) {
+        this.value = Objects.requireNonNull(value);
+    }
+
+    @Override
+    public String value() {
+        return this.value;
+    }
 }
