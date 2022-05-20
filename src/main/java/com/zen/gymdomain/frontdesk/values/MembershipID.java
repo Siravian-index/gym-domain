e0bce364bd0f3a28b8e2cefecea1244e7a561d99
David <depch47@gmail.com>
2022-05-20 10:31:43 -0500

    Implementing VO in FrontDesk

--- src/main/java/com/zen/gymdomain/frontdesk/values/MembershipID.java
+++ src/main/java/com/zen/gymdomain/frontdesk/values/MembershipID.java
@@ -1,4 +1,16 @@
 package com.zen.gymdomain.frontdesk.values;
 
-public class MembershipID {
+import co.com.sofka.domain.generic.Identity;
+
+public class MembershipID extends Identity {
+    public MembershipID() {
+    }
+
+    private MembershipID(String id) {
+        super(id);
+    }
+
+    public static MembershipID of(String id) {
+        return new MembershipID(id);
+    }
 }
--- src/main/java/com/zen/gymdomain/frontdesk/values/MerchandiseID.java
+++ src/main/java/com/zen/gymdomain/frontdesk/values/MerchandiseID.java
@@ -1,4 +1,16 @@
 package com.zen.gymdomain.frontdesk.values;
 
-public class MerchandiseID {
+import co.com.sofka.domain.generic.Identity;
+
+public class MerchandiseID extends Identity {
+    public MerchandiseID() {
+    }
+
+    private MerchandiseID(String id) {
+        super(id);
+    }
+
+    public static MerchandiseID of(String id) {
+        return new MerchandiseID(id);
+    }
 }
