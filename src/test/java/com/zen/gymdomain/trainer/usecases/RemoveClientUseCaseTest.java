48218b021ccfe2197c91ceeb903d8f1182a4aa04
David <depch47@gmail.com>
2022-05-21 10:26:56 -0500

    ADD---tests

--- src/test/java/com/zen/gymdomain/trainer/usecases/RemoveClientUseCaseTest.java
+++ src/test/java/com/zen/gymdomain/trainer/usecases/RemoveClientUseCaseTest.java
@@ -5,8 +5,10 @@ import co.com.sofka.business.repository.DomainEventRepository;
 import co.com.sofka.business.support.RequestCommand;
 import co.com.sofka.domain.generic.DomainEvent;
 import com.zen.gymdomain.trainer.commands.RemoveClient;
+import com.zen.gymdomain.trainer.commands.UpdateClientFitnessLevel;
 import com.zen.gymdomain.trainer.entities.Client;
 import com.zen.gymdomain.trainer.events.ClientAdded;
+import com.zen.gymdomain.trainer.events.ClientFitnessLevelUpdated;
 import com.zen.gymdomain.trainer.events.ClientRemoved;
 import com.zen.gymdomain.trainer.events.TrainerCreated;
 import com.zen.gymdomain.trainer.values.*;
@@ -15,6 +17,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
@@ -33,26 +36,28 @@ class RemoveClientUseCaseTest {
 
     @Test
     void removeClientFromTrainerSuccessfully() {
-//        given
-        TrainerID trainerID = TrainerID.of("fakeTrainerID");
-        ClientID clientID = ClientID.of("fakeClientID");
-        var command = new RemoveClient(trainerID, clientID);
 
-        when(repository.getEventsBy("fakeTrainerID")).thenReturn(List.of(
+        TrainerID fakeTrainerID = TrainerID.of("fakeTrainerID");
+        ClientID fakeClientID = ClientID.of("fakeClientID");
+
+        var command = new RemoveClient(fakeTrainerID, fakeClientID);
+
+        Mockito.when(repository.getEventsBy("fakeTrainerID")).thenReturn(List.of(
                 new TrainerCreated(new Name("Juan")),
-                new ClientAdded(clientID, new Name("david"), new FitnessLevel(FitnessLevelEnum.HIGH), new PhoneNumber("3119878788")),
-                new ClientAdded(ClientID.of("anotherID"), new Name("carlos"), new FitnessLevel(FitnessLevelEnum.MEDIUM), new PhoneNumber("3113434543"))
+                new ClientAdded(ClientID.of("fakeClientID"), new Name("david"), new FitnessLevel(FitnessLevelEnum.MEDIUM), new PhoneNumber("312987657")),
+                new ClientAdded(ClientID.of("anotherClient"), new Name("Luis"), new FitnessLevel(FitnessLevelEnum.MEDIUM), new PhoneNumber("312777757"))
         ));
+
         useCase.addRepository(repository);
-//        when
-        var events = UseCaseHandler.getInstance()
-                .setIdentifyExecutor(command.getTrainerID().value())
+
+        List<DomainEvent> domainEvents = UseCaseHandler.getInstance()
+                .setIdentifyExecutor("fakeTrainerID")
                 .syncExecutor(useCase, new RequestCommand<>(command))
                 .orElseThrow()
                 .getDomainEvents();
-//        assert
-        var event = (ClientRemoved) events.get(0);
-        assertEquals(event.getClientID(), clientID);
+
+        var event = (ClientRemoved) domainEvents.get(0);
+        assertEquals("fakeClientID", event.getClientID().value());
     }
 
 }
