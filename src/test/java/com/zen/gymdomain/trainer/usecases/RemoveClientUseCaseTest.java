928f2d03c90b9e9469d637a19ac83b734274a04d
David <depch47@gmail.com>
2022-05-20 19:10:16 -0500

    ADD---RemoveClientUseCase in Trainer

---
+++ src/test/java/com/zen/gymdomain/trainer/usecases/RemoveClientUseCaseTest.java
@@ -0,0 +1,12 @@
+package com.zen.gymdomain.trainer.usecases;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemoveClientUseCaseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
